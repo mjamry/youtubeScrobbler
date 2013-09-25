15313ef7662f6c3f36daa64e860381d692374cf8
mjamry <michal.jamry@gmail.com>
2013-09-25 20:54:09 +0200

    UICore class has been added. It contains core functionalities related with user interface

--- js/UICore/UIConstants.js
+++ js/UICore/UIConstants.js
@@ -8,22 +8,3 @@ window.UI.UIConstants =
     TimeElapsedContainer: "#player .time"
 }
 
-window.UI.UICore = function(){};
-
-window,UI.UICore.prototype =
-{
-    getPlayerContainer: function()
-    {
-        return $(window.UI.UIConstants.PlayerContainer);
-    },
-
-    getPlaylistContainer: function()
-    {
-        return $(window.UI.UIConstants.PlaylistContainer);
-    },
-
-    getTimeElapsedContainer: function()
-    {
-        return $(window.UI.UIConstants.TimeElapsedContainer);
-    }
-}
---
+++ js/UICore/UICore.js
@@ -0,0 +1,22 @@
+//namespace
+window.UI = window.UI || {};
+
+window.UI.UICore = function(){};
+
+window,UI.UICore.prototype =
+{
+    getPlayerContainer: function()
+    {
+        return $(window.UI.UIConstants.PlayerContainer);
+    },
+
+    getPlaylistContainer: function()
+    {
+        return $(window.UI.UIConstants.PlaylistContainer);
+    },
+
+    getTimeElapsedContainer: function()
+    {
+        return $(window.UI.UIConstants.TimeElapsedContainer);
+    }
+}
