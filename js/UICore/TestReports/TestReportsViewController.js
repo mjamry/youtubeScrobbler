a577041857609fbbbc8bfa4a7e91f6912723385a
mjamry <michal.jamry@gmail.com>
2013-10-28 20:50:03 +0100

    Added form to send bug reports. First shot to add new controller - which will control test reports. Also some UI classes have been moved to separate folders.

---
+++ js/UICore/TestReports/TestReportsViewController.js
@@ -0,0 +1,12 @@
+//namespace
+window.UI = window.UI || {};
+
+window.UI.TestReportsViewController = function(){};
+
+window.UI.TestReportsViewController.prototype =
+{
+    initialise: function()
+    {
+
+    }
+}
