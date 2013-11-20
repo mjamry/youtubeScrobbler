ad13d1b21ecdb1d9b9d41aeb04444d4d9b7d6c6e
mjamry <michal.jamry@gmail.com>
2013-11-20 19:02:07 +0100

    Added templates of UI classes.

---
+++ js/UICore/PlaylistControl/PlaylistControlViewController.js
@@ -0,0 +1,14 @@
+//namespace
+window.UI = window.UI || {};
+
+window.UI.PlaylistControlViewController = function(model, view, config)
+{
+    this.model = model;
+    this.config = config;
+    this.view = $("#"+view);
+}
+
+window.UI.PlaylistControlViewController.prototype =
+{
+
+}
