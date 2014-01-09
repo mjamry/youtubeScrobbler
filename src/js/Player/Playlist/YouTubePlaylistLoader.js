421425424d29bc9eadc4969f55e1fedc1b8a507d
mjamry <michal.jamry@gmail.com>
2014-01-09 20:59:32 +0100

    Changes in youtube playlist loader. Now it is possible to create playlist containing all items from youtube.

--- src/js/Player/Playlist/YouTubePlaylistLoader.js
+++ src/js/Player/Playlist/YouTubePlaylistLoader.js
@@ -9,12 +9,14 @@ window.Player.YouTubePlaylistConstant =
     API_URL: "https://gdata.youtube.com/feeds/api/",
     API_VERSION: 2,
     FEED_PARAMS: "alt=jsonc&v=2",
+    FEED_QUANTITY_PARAMS: "&max-results=50&start-index=",
     PLAYLIST_PARAMETER_NAME: "list",
     VIDEO_PARAMETER_NAME: "v",
     PLAYLIST_API_VALUE: "playlists/",
     VIDEOS_API_VALUE: "videos/",
     VIDEO_TITLE_ID: 1,
     ARTIST_NAME_ID: 0,
+    MAX_NUMBER_OF_RESULTS: 50,
     VIDEO_NAME_SEPARATOR: "-",
     MEDIA_TYPE: "video/youtube",
     REGEX_NAMING_PATTERN: "([^\\-]*)-\\s?((?:[^\\{\\}\\(\\)\\[\\]]?)*)(.*)"
@@ -71,36 +73,66 @@ window.Player.YouTubePlaylistLoader.prototype =
         return mediaDetails;
     },
 
-    //gets playlist using playlist id. When finished it calls callback function to return data.
-    _createPlaylist : function(id, callback)
+    //creates playlist using details obtained from youtube
+    _createPlaylist: function(list)
     {
+        var playlist = new window.Player.Playlist();
+        for (var i = 0; i < list.length; i++)
+        {
+            var mediaDetails = this._getMediaDetails(list[i].video);
+
+            if (mediaDetails !== null)
+            {
+                playlist.addItem(mediaDetails);
+            }
+        }
+
+        return playlist;
+    },
+
+    //gets playlist using playlist id. When finished it calls callback function to return data.
+    _obtainPlaylistDetails : function(id, callback)
+    {
+        var playlist = new window.Player.Playlist();
         var url = window.Player.YouTubePlaylistConstant.API_URL +
             window.Player.YouTubePlaylistConstant.PLAYLIST_API_VALUE +
             id +
             window.Common.UrlParserConstants.PARAMS_START_SIGN +
-            window.Player.YouTubePlaylistConstant.FEED_PARAMS;
+            window.Player.YouTubePlaylistConstant.FEED_PARAMS +
+            window.Player.YouTubePlaylistConstant.FEED_QUANTITY_PARAMS;
 
-        $.getJSON(url, $.proxy(function(result)
+        //TODO i belieave that it can be done in better way
+        (function __obtainPlaylistDetails(playlist, callback, that)
         {
-           var playlist = new window.Player.Playlist();
-           var list = result.data.items;
-           for(var i=0;i<list.length;i++)
-           {
-                var mediaDetails = this._getMediaDetails(list[i].video);
-
-                if(mediaDetails !== null)
+            return function getPlaylistFromYoutube(startingIndex)
+            {
+                $.getJSON(url+startingIndex, function(result)
                 {
-                    playlist.add(mediaDetails);
-                }
-           }
+                    if(result.data.items)
+                    {
+                        var list = result.data.items;
+                        playlist.addPlaylist(that._createPlaylist(list));
 
-           callback(playlist);
-        }, this));
+                        //check if all videos details are obtained if not increment start_index and call this function once again
+                        //at the end call callback passing created playlist
+                        if(result.data.items.length >= window.Player.YouTubePlaylistConstant.MAX_NUMBER_OF_RESULTS)
+                        {
+                            getPlaylistFromYoutube(startingIndex + window.Player.YouTubePlaylistConstant.MAX_NUMBER_OF_RESULTS);
+                        }
+                        else
+                        {
+                            callback(playlist);
+                        }
+                    }
+                });
+            }
+        })(playlist, callback, this)(1);
     },
             
-    //gets video data using id. When finished it calls callback function to return data.
-    _loadVideo : function(id, callback)
+    //gets video data using id. When finish it calls callback function to return data.
+    _obtainVideoDetails : function(id, callback)
     {
+        var playlist = null;
         var url = window.Player.YouTubePlaylistConstant.API_URL +
             window.Player.YouTubePlaylistConstant.VIDEOS_API_VALUE +
             id +
@@ -109,23 +141,17 @@ window.Player.YouTubePlaylistLoader.prototype =
 
         $.getJSON(url, $.proxy(function(result)
         {
-            var mediaDetails = this._getMediaDetails(result.data);
-
-            var playlist = new window.Player.Playlist();
-
-            if(mediaDetails !== null)
-            {
-                playlist.add(mediaDetails);
-            }
-
+            //create a table from result
+            playlist = this._createPlaylist([{video:result.data}]);
             //TODO what if playlist is empty?
-			callback(playlist);
-           
-        }, this));
+            callback(playlist);
+        },
+        this));
     },
     
     //parses specified url address (form YT). Depending on url structure it loads playlist or single video.
     //returns playlist object literal: playlist = {title:string, videos:[{id, title}]};
+    //playlist is returned via callback function
     loadPlaylistFromUrl : function(url, callback)
     {
         Logger.getInstance().Debug("Sending request to YouTube. Url: "+url);
@@ -134,14 +160,14 @@ window.Player.YouTubePlaylistLoader.prototype =
         
         if(playlistId !== window.Common.UrlParserConstants.URL_PARSE_ERR)
         {
-            this._createPlaylist(playlistId, callback);
+            this._obtainPlaylistDetails(playlistId, callback);
         }
         else
         {
             var videoId = parser.getParameterValue(url, window.Player.YouTubePlaylistConstant.VIDEO_PARAMETER_NAME);
             if(videoId !== window.Common.UrlParserConstants.URL_PARSE_ERR)
             {
-                this._loadVideo(videoId, callback);
+                this._obtainVideoDetails(videoId, callback);
             }
         }
     }
