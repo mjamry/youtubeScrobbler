867852054af7017a2345769f1d675f80ec7f50d5
mjamry <michal.jamry@gmail.com>
2014-01-18 21:33:45 +0100

    Fix to broken mediaDetails class.

--- src/js/Player/Media/MediaDetails.js
+++ src/js/Player/Media/MediaDetails.js
@@ -2,29 +2,29 @@
 window.Player = window.Player || {};
 
 //DTO - contains details specific for video/audio object.
-window.Player.MediaDetails = function(){};
-
-window.Player.MediaDetails.prototype =
+window.Player.MediaDetails = function()
 {
-    artist:
-    {
-        name: "",
+    return {
+        artist:
+        {
+            name: "",
+            mbid: "",
+            url: ""
+        },
+        title: "",
         mbid: "",
-        url: ""
-    },
-    title: "",
-    mbid: "",
-    duration: "", //new Druation()
-    url: "",
-    mediaType: "",//type of media e.g. "video/youtube"
-    id: "",       //media id from portal e.g lastFm - used to indicate if details has been already obtained
-    album:
-    {
-        name: "",
-        mbid: "",
+        duration: "", //new Druation()
         url: "",
-        cover: ""
-    },
-    loved: "",       //determine if track has been loved already
-    tags: ""
+        mediaType: "",//type of media e.g. "video/youtube"
+        id: "",       //media id from portal e.g lastFm - used to indicate if details has been already obtained
+        album:
+        {
+            name: "",
+            mbid: "",
+            url: "",
+            cover: ""
+        },
+        loved: "",       //determine if track has been loved already
+        tags: ""
+    };
 };
