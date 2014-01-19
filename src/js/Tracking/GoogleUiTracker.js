a51268542539436f34b581e3eabef2838d899f26
mjamry <michal.jamry@gmail.com>
2014-01-19 17:58:56 +0100

    Change in tracking logic - changes needed to have a possibility to separate user actions depending on area (playlist control, playback control etc.)

--- src/js/Tracking/GoogleUiTracker.js
+++ src/js/Tracking/GoogleUiTracker.js
@@ -12,31 +12,31 @@ window.Tracking.GoogleUiTracker.prototype =
     {
         $(playbackConfig.PlayButton).click($.proxy(function handlePlayClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaybackControl+"_play");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaybackControl, "play");
         },
         this));
 
         $(playbackConfig.PauseButton).click($.proxy(function handlePauseClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaybackControl+"_pause")
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaybackControl, "pause")
         },
         this));
 
         $(playbackConfig.NextButton).click($.proxy(function handleNextClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaybackControl+"_next");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaybackControl, "next");
         },
         this));
 
         $(playbackConfig.PreviousButton).click($.proxy(function handlePreviousClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaybackControl+"_previous");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaybackControl, "previous");
         },
         this));
 
         $(playbackConfig.VolumeButton).click($.proxy(function handleVolumeClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaybackControl+"_volume");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaybackControl, "volume");
         },
         this));
     },
@@ -45,25 +45,25 @@ window.Tracking.GoogleUiTracker.prototype =
     {
         $(playlistConfig.ClearButton).click($.proxy(function handleClearPlaylistClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_clear");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaylistControl, "clear");
         },
         this));
 
         $(playlistConfig.SaveButton).click($.proxy(function handleSavePlaylistClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_save");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaylistControl, "save");
         },
         this));
 
         $(playlistConfig.ShuffleButton).click($.proxy(function handleShufflePlaylistClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_shuffle");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaylistControl, "shuffle");
         },
         this));
 
         $(playlistConfig.RepeatButton).click($.proxy(function handleRepeatPlaylistClick()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_repeat");
+            GoogleTracker.getInstance().trackUiAction(this.config.PlaylistControl, "repeat");
         },
         this));
     },
@@ -72,13 +72,25 @@ window.Tracking.GoogleUiTracker.prototype =
     {
         $(testConfig.ErrorButton).click($.proxy(function handleErrorReportOpen()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.TestEnvironment+"_error_report");
+            GoogleTracker.getInstance().trackUiAction(this.config.TestEnvironment, "open_error_report");
         },
         this));
 
         $(testConfig.FeatureButton).click($.proxy(function handleFeatureRequestOpen()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.TestEnvironment+"_feature_request");
+            GoogleTracker.getInstance().trackUiAction(this.config.TestEnvironment, "open_feature_request");
+        },
+        this));
+
+        $(testConfig.ShowFormButton).click($.proxy(function handleTestEnvironmentOpen()
+        {
+            GoogleTracker.getInstance().trackUiAction(this.config.TestEnvironment, "open_test_environment");
+        },
+        this));
+
+        $(testConfig.HideFormButton).click($.proxy(function handleTestEnvironmentClose()
+        {
+            GoogleTracker.getInstance().trackUiAction(this.config.TestEnvironment, "close_test_environment");
         },
         this));
     },
@@ -87,13 +99,13 @@ window.Tracking.GoogleUiTracker.prototype =
     {
         $(loggerConfig.ClearLogsButtonId).click($.proxy(function handleLogsClear()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.LoggerControl+"_clear");
+            GoogleTracker.getInstance().trackUiAction(this.config.LoggerControl, "clear");
         },
         this));
 
         $(loggerConfig.LoggerLevelComboBox).change($.proxy(function handleLoggerLevelChange()
         {
-            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.LoggerControl+"_clear", $(loggerConfig.LoggerLevelComboBox).val());
+            GoogleTracker.getInstance().trackUiAction(this.config.LoggerControl, "level_changed", $(loggerConfig.LoggerLevelComboBox).val());
         },
         this));
     },
