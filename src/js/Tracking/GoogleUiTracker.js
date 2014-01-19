0f25d08a50a25678d3889042a221604d64a3118c
mjamry <michal.jamry@gmail.com>
2014-01-19 12:28:12 +0100

    Added tracking to playlist control buttons.

--- src/js/Tracking/GoogleUiTracker.js
+++ src/js/Tracking/GoogleUiTracker.js
@@ -43,7 +43,29 @@ window.Tracking.GoogleUiTracker.prototype =
 
     hookUpToPlaylistControlEvents: function(playlistConfig)
     {
+        $(playlistConfig.ClearButton).click($.proxy(function handleClearPlaylistClick()
+        {
+            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_clear");
+        },
+        this));
 
+        $(playlistConfig.SaveButton).click($.proxy(function handleSavePlaylistClick()
+        {
+            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_save");
+        },
+        this));
+
+        $(playlistConfig.ShuffleButton).click($.proxy(function handleShufflePlaylistClick()
+        {
+            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_shuffle");
+        },
+        this));
+
+        $(playlistConfig.RepeatButton).click($.proxy(function handleRepeatPlaylistClick()
+        {
+            GoogleTracker.getInstance().trackUiAction(this.config.ClickAction, this.config.PlaylistControl+"_repeat");
+        },
+        this));
     },
 
     hookUpToTestControlEvents: function(testConfig)
