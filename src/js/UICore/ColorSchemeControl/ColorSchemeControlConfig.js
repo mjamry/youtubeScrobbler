935680bc8ec0db39137c545b76340c770e542b99
mjamry <michal.jamry@gmail.com>
2014-02-19 14:06:19 +0100

    Changes in styles for color scheme control buttons.

--- src/js/UICore/ColorSchemeControl/ColorSchemeControlConfig.js
+++ src/js/UICore/ColorSchemeControl/ColorSchemeControlConfig.js
@@ -1,7 +1,7 @@
 window.UI.ColorSchemeControlConfig =
 {
-    LightSchemeButton: "#color-scheme-control-light",
-    DarkSchemeButton: "#color-scheme-control-dark",
+    LightSchemeButton: "#color-scheme-control-light-button",
+    DarkSchemeButton: "#color-scheme-control-dark-button",
 
     DarkStyle: "css/dark_color_scheme.css",
     LightStyle: "css/light_color_scheme.css"
