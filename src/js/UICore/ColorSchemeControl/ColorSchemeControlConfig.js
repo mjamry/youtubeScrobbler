23a9a2b1e06ee39e6978c298ece47b5f870a9d3a
mjamry <michal.jamry@gmail.com>
2014-02-22 15:45:50 +0100

    Added actions to theme switch button

--- src/js/UICore/ColorSchemeControl/ColorSchemeControlConfig.js
+++ src/js/UICore/ColorSchemeControl/ColorSchemeControlConfig.js
@@ -1,7 +1,6 @@
 window.UI.ColorSchemeControlConfig =
 {
-    LightSchemeButton: "#color-scheme-control-light-button",
-    DarkSchemeButton: "#color-scheme-control-dark-button",
+    ThemeSwitchButton: "#control-button-switch-theme",
 
     DarkStyle: "css/dark_color_scheme.css",
     LightStyle: "css/light_color_scheme.css"
