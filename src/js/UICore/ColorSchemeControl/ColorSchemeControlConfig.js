e1e03b8e48331c55c354bfaf28fbcc096dcd70cd
mjamry <michal.jamry@gmail.com>
2014-03-06 18:55:48 +0100

    Renamed and relocated theme style sheets. Also added to build directory

--- src/js/UICore/ColorSchemeControl/ColorSchemeControlConfig.js
+++ src/js/UICore/ColorSchemeControl/ColorSchemeControlConfig.js
@@ -3,6 +3,6 @@ window.UI.ColorSchemeControlConfig =
     LightSchemeButton: "#color-scheme-control-light-button",
     DarkSchemeButton: "#color-scheme-control-dark-button",
 
-    DarkStyle: "css/dark_color_scheme.css",
-    LightStyle: "css/light_color_scheme.css"
+    DarkStyle: "css/dark.css",
+    LightStyle: "css/light.css"
 };
