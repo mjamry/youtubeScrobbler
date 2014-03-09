b98c46baeb1bbc4e2d34cab8956ea5620ba0c375
mjamry <michal.jamry@gmail.com>
2014-03-09 20:27:53 +0100

    Added code to enter full screen mode

--- src/js/Player/VideoSizeControlService.js
+++ src/js/Player/VideoSizeControlService.js
@@ -8,9 +8,25 @@ window.Player.VideoSizeControl = function(player)
 
 window.Player.VideoSizeControl.prototype =
 {
-    setFullScreenModeOn: function()
+    initialise: function videoSizeControlInitialise()
     {
 
+    },
+
+    setFullScreenModeOn: function()
+    {
+        //var playerContainer = document.getElementById(new window.UI.UIControllersFactory().createUICore().getPlayerContainer());
+        var playerContainer = document.getElementById("fullscreen-video-container");
+        if (playerContainer.requestFullscreen) {
+            playerContainer.requestFullscreen();
+        } else if (playerContainer.webkitRequestFullscreen) {
+            playerContainer.webkitRequestFullscreen();
+        } else if (playerContainer.mozRequestFullScreen) {
+            playerContainer.mozRequestFullScreen();
+        } else if (playerContainer.msRequestFullscreen) {
+            playerContainer.msRequestFullscreen();
+        }
+        this.player.setSize(window.screen.width, window.screen.height);
     },
 
     setFullScreenModeOff: function()
