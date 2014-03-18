c0208f96e67a0a5fa77d5498dc863a5dfc2bc072
mjamry <michal.jamry@gmail.com>
2014-03-18 22:12:49 +0100

    Partial implementation added to the user notificator (UI) class. Also new classes have been added to the index.html file

--- src/js/UICore/UserNotification/UserNotifierConfiguration.js
+++ src/js/UICore/UserNotification/UserNotifierConfiguration.js
@@ -2,5 +2,8 @@ window.UI = window.UI || {};
 
 window.UI.UserNotifierConfiguration =
 {
-
+    NotificationTimeout: 20,
+    MaxNumberOfNotifications: 2,
+    ErrorNotificationClass: "user-notification-error",
+    InfoNotificationClass: "info-notification-error"
 };
--- src/js/UICore/UserNotification/UserNotifierViewController.js
+++ src/js/UICore/UserNotification/UserNotifierViewController.js
@@ -1,11 +1,26 @@
 window,UI = window.UI || {};
 
-window.UI.UserNotifierViewController = function()
+window.UI.UserNotifierViewController = function(view, configuration)
 {
-
+    this.view = view;
+    this.config = configuration;
 };
 
 window.UI.UserNotifierViewController.prototype =
 {
+    _handleInfoNotificationRequest: function(args)
+    {
 
+    },
+
+    _handleErrorNotificationRequest: function(args)
+    {
+
+    },
+
+    initialise: function()
+    {
+        EventBroker.getInstance().addListener(window.Common.UserNotifierEvents.InfoNotificationRequested, $.proxy(this._handleInfoNotificationRequest, this));
+        EventBroker.getInstance().addListener(window.Common.UserNotifierEvents.ErrorNotificationRequested, $.proxy(this._handleErrorNotificationRequest, this));
+    }
 };
