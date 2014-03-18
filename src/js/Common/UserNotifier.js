07b09d90aee43a7a65bd8c8eb4abd72072083605
mjamry <michal.jamry@gmail.com>
2014-03-18 21:01:48 +0100

    Introduced singleton for userNotifier class.

--- src/js/Common/UserNotifier.js
+++ src/js/Common/UserNotifier.js
@@ -1,12 +1,38 @@
 //namespace
 window.Common = window.Common || {};
 
-window.Common.UserNotifier = function()
+UserNotifier = function()
 {
-
+    this._instance = null;
 };
 
-window.Common.UserNotifier =
+UserNotifier.setInstance = function()
+{
+    if(UserNotifier._instance !== null)
+    {
+        var errorMsg = "Instance of UserNotifier has been already set!";
+        Logger.getInstance().Error(errorMsg);
+        throw errorMsg;
+    }
+
+    UserNotifier._instance = instance;
+};
+
+UserNotifier.getInstance = function()
+{
+    if(UserNotifier._instance === null)
+    {
+        var errorMsg = "Instance of UserNotifier has not been set yet!";
+        Logger.getInstance().Error(errorMsg);
+        throw errorMsg;
+    }
+
+    return UserNotifier._instance;
+};
+
+window.Common.UserNotifierImpl = function(){};
+
+window.Common.UserNotifierImpl.prototype =
 {
     info: function showUserInformation(message, undoCallback)
     {
@@ -18,3 +44,4 @@ window.Common.UserNotifier =
 
     }
 };
+
