bb77cdff159e9f7e585ad397ba71c674d7aaa72f
mjamry <michal.jamry@gmail.com>
2014-03-22 08:50:49 +0100

    Added classes names to configuration file

--- src/js/UICore/UserNotification/UserNotifierConfiguration.js
+++ src/js/UICore/UserNotification/UserNotifierConfiguration.js
@@ -5,5 +5,11 @@ window.UI.UserNotifierConfiguration =
     NotificationTimeout: 20,
     MaxNumberOfNotifications: 2,
     ErrorNotificationClass: "user-notification-error",
-    InfoNotificationClass: "info-notification-error"
+    InfoNotificationClass: "info-notification-error",
+
+    NotificationIconContainer: "user-notification-icon",
+    NotificationMessageContainer: "user-notification-message",
+
+    UndoButton: "user-notification-undo-button",
+    CloseButton: "user-notification-close-button"
 };
