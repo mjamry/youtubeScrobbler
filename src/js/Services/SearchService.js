6cd1bf88fad3487a29d2023fe51b0661c19e9ec8
mjamry <michal.jamry@gmail.com>
2014-07-22 11:05:52 +0200

    New method has been added to the search service to generate correct url for search item.

--- src/js/Services/SearchService.js
+++ src/js/Services/SearchService.js
@@ -2,6 +2,11 @@ window.Services = window.Services || {};
 
 window.Services.SearchService = function()
 {
+    this.RESOURCE_TYPE_PLAYLIST = "youtube#playlist";
+    this.RESOURCE_TYPE_VIDEO = "youtube#video";
+    this.URL_VIDEO = window.Google.GoogleApiConstants.YOUTUBE.URL;
+    this.URL_PLAYLIST = window.Google.GoogleApiConstants.YOUTUBE.URL+"0&"+window.Google.GoogleApiConstants.YOUTUBE.LINK_PARAMS.PLAYLIST+"=";
+
     this.dataProviders =
     {
         Youtube: new window.Google.GoogleApiWrapper()
@@ -10,29 +15,41 @@ window.Services.SearchService = function()
 
 window.Services.SearchService.prototype =
 {
+    //gets url depending on resource type
+    _getUrlForResource: function(details)
+    {
+        if(details.id.kind == this.RESOURCE_TYPE_PLAYLIST)
+        {
+            return this.URL_PLAYLIST+details.id.playlistId;
+        }
+        else if(details.id.kind == this.RESOURCE_TYPE_VIDEO)
+        {
+            return this.URL_VIDEO+details.id.videoId
+        }
+    },
+
     _handleResult: function(result)
     {
         var output = [];
+        var that = this;
         result.items.forEach(function(details)
         {
 
             //TODO: filter titles using naming pattern etc
             output.push(
                 {
-                    //TODO check if result is a video or playlist and create appropriate url
-                    url: window.Google.GoogleApiConstants.YOUTUBE.URL+details.id.videoId,
+                    url: that._getUrlForResource(details),
                     title: details.snippet.title
                 }
             );
         });
 
         EventBroker.getInstance().fireEventWithData(window.Services.SearchResultEvents.SearchFinishedWithSuccess, output);
-
     },
 
     search: function(value)
     {
-        this.dataProviders.Youtube.getSearchResults({q:value}, this._handleResult);
+        this.dataProviders.Youtube.getSearchResults({q:value}, this._handleResult.bind(this));
     }
 };
 
