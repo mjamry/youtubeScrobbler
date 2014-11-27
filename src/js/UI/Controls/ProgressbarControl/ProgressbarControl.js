cbcd5f78f0a690a0e9e8faa5356fda429c7289ad
mjamry <michal.jamry@gmail.com>
2014-11-27 21:02:09 +0100

    Added timeout to progressbar. Now it will be hidden after timeout (without update).

--- src/js/UI/Controls/ProgressbarControl/ProgressbarControl.js
+++ src/js/UI/Controls/ProgressbarControl/ProgressbarControl.js
@@ -5,17 +5,33 @@ window.UI.ProgressbarControl = function(controlClass, title)
 {
     this.container = $("#controls-schemes .progressbar").clone();
     this.container.find(".progressbar-title").text(title);
+
+    this.timer = 0;
+    this._setTimeout();
 };
 
 window.UI.ProgressbarControl.prototype =
 {
+    _setTimeout: function()
+    {
+        this.timer = setTimeout(this.hide.bind(this), window.UI.ProgressbarControlConfiguration.Timeout);
+    },
+
+    _refreshTimeout: function()
+    {
+        clearTimeout(this.timer);
+        this._setTimeout();
+    },
+
     update: function(value)
     {
-        this.container.find(".progressbar-body").width(value+"%");
+        this.container.find(window.UI.ProgressbarControlConfiguration.ProgressbarBody).width(value+"%");
+        this._refreshTimeout();
     },
 
     hide: function()
     {
         this.container.hide();
+        clearTimeout(this.timer);
     }
 };
