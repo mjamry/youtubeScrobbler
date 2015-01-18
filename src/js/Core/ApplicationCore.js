8d6015c8e7fa0019ca9cd30fc8256ccb9fef4a68
mjamry <michal.jamry@gmail.com>
2015-01-18 10:46:47 +0100

    Code cleanup changes in application core.

--- src/js/Core/ApplicationCore.js
+++ src/js/Core/ApplicationCore.js
@@ -5,9 +5,7 @@ window.ApplicationCore = window.ApplicationCore || {};
 window.UI = window.UI || {};
 window.Player = window.Player || {};
 
-window.ApplicationCore.AppCore = function()
-{
-};
+window.ApplicationCore.AppCore = function(){};
 
 window.ApplicationCore.AppCore.prototype =
 {
@@ -23,24 +21,15 @@ window.ApplicationCore.AppCore.prototype =
         this.playlistDetailsProvider = playerServicesFactory.createPlaylistDetailsProvider(this.playlistService);
         this.player = coreServicesFactory.createMediaPlayer(this.playlistDetailsProvider);
         this.onlineScrobbler = coreServicesFactory.createOnlineScrobbler(lastFmServicesFactory.createScrobbler());
-
         this.googleApiWrapper = coreServicesFactory.createGoogleApiWrapper();
-
         this.playlistLoaderService = coreServicesFactory.createPlaylistLoaderService(this.playlistService, this.googleApiWrapper);
         this.searchService = coreServicesFactory.createSearchService(this.googleApiWrapper);
-
         this.playlistFlowController = playerServicesFactory.createPlaylistFlowController(this.playlistService);
-
-
         this.playbackDetailsService = coreServicesFactory.createPlaybackDetailsService(this.player);
-
         this.sessionService = coreServicesFactory.createSessionService(this.googleApiWrapper);
         this.sessionService.initialise();
-
         this.playbackControlService = coreServicesFactory.createPlaybackControlService(this.player, this.playlistFlowController);
-
         this.playlistElementLoveStateModifier = playerServicesFactory.createPlaylistElementLoveStateModifier(this.playlistService, lastFmServicesFactory.createTrackLoveStateModifier());
-
         this.welcomeScreenService = coreServicesFactory.createWelcomeService();
     },
 
@@ -72,7 +61,7 @@ window.ApplicationCore.AppCore.prototype =
         this.scrobblingControlViewController = uiFactory.createScrobblingControlViewController(this.onlineScrobbler);
     },
 
-    initialiseViewControllers: function(menuItemsConfiguration)
+    initialiseViewControllers: function(menuConfig)
     {
         this.playlistViewController.initialise();
         this.playbackDetailsViewController.initialise();
@@ -89,14 +78,19 @@ window.ApplicationCore.AppCore.prototype =
         this.menuController.initialise();
         this.scrobblingControlViewController.initialise();
 
-        for(var item in menuItemsConfiguration)
-        {
-            this.menuController.add(menuItemsConfiguration[item].Name, menuItemsConfiguration[item].Icon, menuItemsConfiguration[item].Page, menuItemsConfiguration[item].Position);
-        }
+        this._initialiseMenu(menuConfig);
 
         if(this.welcomeScreenService.isApplicationAlreadyActivated())
         {
             this.welcomeScreenController.showMainScreen();
+        }
+    },
+
+    _initialiseMenu: function(menuItemsConfiguration)
+    {
+        for(var item in menuItemsConfiguration)
+        {
+            this.menuController.add(menuItemsConfiguration[item].Name, menuItemsConfiguration[item].Icon, menuItemsConfiguration[item].Page, menuItemsConfiguration[item].Position);
         }
     }
 };
