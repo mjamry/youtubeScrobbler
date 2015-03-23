d1cd2b0017af5b32124c34c261612fc373ff4b5e
mjamry <michal.jamry@gmail.com>
2015-03-23 20:50:36 +0100

    Added body to the contextMenu classes.

--- src/js/UI/Controls/ContextMenu/ContextMenu.js
+++ src/js/UI/Controls/ContextMenu/ContextMenu.js
@@ -1,11 +1,50 @@
 window.UI = window.UI || {};
 
-window.UI.ContextMenu = function()
+window.UI.ContextMenu = function(config)
 {
-
+    this.config = config;
+    this.body = document.createElement('div');
+    this.body.className += this.config.ContextMenuBody;
 };
 
 window.UI.ContextMenu.prototype =
 {
+    _hideMenu: function()
+    {
+        this.body.hide();
+    },
 
+    _createIcon: function(iconClass)
+    {
+        var icon = document.createElement("i");
+        icon.className += iconClass;
+
+        return icon;
+    },
+
+    _handleClickAction: function(that, handler)
+    {
+        return new function (e)
+        {
+            e.stopPropagation();
+            handler.call();
+            that._hideMenu();
+        };
+    },
+
+    addItem: function(icon, text, action)
+    {
+        var newMenuItem = $("#controls-schemes .context-menu-item").clone();
+        newMenuItem.find(this.config.ContextMenuItemIcon).append(this._createIcon(icon));
+        newMenuItem.find(this.config.ContextMenuItemTitle).append(text);
+
+        newMenuItem.click(this._handleClickAction(this, action));
+
+        this.body.appendChild(newMenuItem);
+    },
+
+    buildMenu: function()
+    {
+        return this.body;
+    }
 };
--- src/js/UI/Controls/ContextMenu/ContextMenuConfiguration.js
+++ src/js/UI/Controls/ContextMenu/ContextMenuConfiguration.js
@@ -2,5 +2,8 @@ window.UI = window.UI || {};
 
 window.UI.ContextMenuConfiguration =
 {
+    ContextMenuItemTitle: ".context-menu-item-title",
+    ContextMenuItemIcon: ".context-menu-item-icon",
 
+    ContextMenuBody: ".context-menu-body"
 };
