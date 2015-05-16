8ebb1d898de0c71d3cea68ab8b2c69e783534d44
mjamry <michal.jamry@gmail.com>
2015-05-16 22:01:23 +0200

    New styles for playlist details items.

--- src/js/UICore/PlaylistManage/PlaylistManageViewController.js
+++ src/js/UICore/PlaylistManage/PlaylistManageViewController.js
@@ -12,5 +12,24 @@ window.UI.PlaylistManageViewController.prototype =
     initialise: function()
     {
         var playlists = this.playlistManager.loadPlaylistDetailsForAllPlaylists("Local");
+        var plNames = Object.keys(playlists);
+        plNames.forEach(function(key)
+        {
+            var plDetails = playlists[key];
+            var playlistDetails = $("#controls-schemes "+this.config.PlaylistDetailsContainer).clone();
+            playlistDetails.find(this.config.PlaylistName).append(plDetails.name);
+            playlistDetails.find(this.config.PlaylistDescr).append(plDetails.name);
+            playlistDetails.find(this.config.PlaylistItemsCount).append(plDetails.name);
+            playlistDetails.find(this.config.PlaylistStorage).append(plDetails.name);
+
+
+            this.view.append(playlistDetails);
+
+        }.bind(this));
+
+
+
+
+
     }
 };
