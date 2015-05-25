1ab87465d156f0fc60197d9dd9021db80a7490c2
mjamry <michal.jamry@gmail.com>
2015-05-25 21:47:32 +0200

    Some css modifications.

--- src/js/UICore/PlaylistManage/PlaylistManageConfiguration.js
+++ src/js/UICore/PlaylistManage/PlaylistManageConfiguration.js
@@ -3,7 +3,7 @@ window.UI = window.UI || {};
 window.UI.PlaylistManageConfiguration =
 {
     PlaylistDetailsContainer: ".playlist-details-view",
-    PlaylistName: ".playlist-details-name",
+    PlaylistName: ".playlist-details-header",
     PlaylistId: ".playlist-details-id",
     PlaylistDescr: ".playlist-details-description",
     PlaylistItemsCount: ".playlist-details-items-count",
