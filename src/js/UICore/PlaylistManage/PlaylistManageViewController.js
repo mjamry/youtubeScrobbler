bfbab0d16c604d6d5554bbc0322a488581c7be3e
mjamry <michal.jamry@gmail.com>
2015-05-25 21:06:58 +0200

    First part of changes needed to use listjs in playlist manager view.

--- src/js/UICore/PlaylistManage/PlaylistManageViewController.js
+++ src/js/UICore/PlaylistManage/PlaylistManageViewController.js
@@ -5,6 +5,15 @@ window.UI.PlaylistManageViewController = function(config, view, playlistManager)
     this.view = view;
     this.config = config;
     this.playlistManager = playlistManager;
+
+    var options =
+    {
+        valueNames: ["playlist-details-name", "playlist-details-id", "playlist-details-description"],
+        //html() returns only inside of element, to there is a need to wrap everything in div and get parent's html
+        item: $("#controls-schemes "+this.config.PlaylistDetailsContainer).clone().wrap("<div />").parent().html()
+    };
+
+    this.list = new List("playlist-manager", options);
 };
 
 window.UI.PlaylistManageViewController.prototype =
@@ -32,20 +41,39 @@ window.UI.PlaylistManageViewController.prototype =
     {
         var playlistsDetails = this.playlistManager.loadPlaylistDetailsForAllPlaylists("Local");
 
+
+
+        this.list.add({
+            "playlist-details-name": "a",
+            "playlist-details-id": "b",
+            "playlist-details-description": "c"
+        });
+
         playlistsDetails.forEach(function(plDetails)
         {
-            var playlistDetails = $("#controls-schemes "+this.config.PlaylistDetailsContainer).clone();
-            playlistDetails.find(this.config.PlaylistName).append(plDetails.name);
-            playlistDetails.find(this.config.PlaylistId).append(plDetails.id);
-            playlistDetails.find(this.config.PlaylistDescr).append(plDetails.description);
-            playlistDetails.find(this.config.PlaylistItemsCount).append(plDetails.playlist.length());
-            playlistDetails.find(this.config.PlaylistStorage).append(plDetails.storageType);
+            var item = {};
+            item[this.config.PlaylistName] = plDetails.name;
+            item[this.config.PlaylistId] = plDetails.id;
+            item[this.config.PlaylistDescr] = plDetails.description;
+            item[this.config.PlaylistItemsCount] = plDetails.playlist.length();
+            item[this.config.PlaylistStorage] = plDetails.storageType;
 
-            this.view.append(playlistDetails);
-
-            playlistDetails.click(this._hookUpToClickAction(this));
+            this.list.add(item);
+            //var playlistDetails = $("#controls-schemes "+this.config.PlaylistDetailsContainer).clone();
+            //playlistDetails.find().append(plDetails.name);
+            //playlistDetails.find(this.config.PlaylistId).append(plDetails.id);
+            //playlistDetails.find(this.config.PlaylistDescr).append(plDetails.description);
+            //playlistDetails.find(this.config.PlaylistItemsCount).append(plDetails.playlist.length());
+            //playlistDetails.find(this.config.PlaylistStorage).append(plDetails.storageType);
+            //
+            //this.view.find("ul").append(playlistDetails);
+            //
+            //playlistDetails.click(this._hookUpToClickAction(this));
 
         }.bind(this));
+
+
+
     },
 
     initialise: function()
