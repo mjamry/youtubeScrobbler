4e70e3ea7ab42dd76f2179fefc9626f5ac451fc1
mjamry <michal.jamry@gmail.com>
2015-07-14 20:14:51 +0200

    Updated changelog for new app version. Minor crrection to the changelog update script.

--- updateChangelogIssues.py
+++ updateChangelogIssues.py
@@ -3,6 +3,6 @@ import re
 
 linkHref = "https://github.com/mjamry/youtubeScrobbler/issues/"
  
-for line in fileinput.input("CHANGELOG.html",inplace=1):
+for line in fileinput.input("src/CHANGELOG.html",inplace=1):
     line = re.sub(r'#([0-9]+)', r'<a target="_blank" href="https://github.com/mjamry/youtubeScrobbler/issues/\1">#\1</a>', line.rstrip())
     print(line)
