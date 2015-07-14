0349eaf99ac500a1596026f3c66eac35e1c85086
mjamry <michal.jamry@gmail.com>
2015-07-14 20:23:28 +0200

    Removed redundant links from the changelog file. Updated script.

--- updateChangelogIssues.py
+++ updateChangelogIssues.py
@@ -4,5 +4,5 @@ import re
 linkHref = "https://github.com/mjamry/youtubeScrobbler/issues/"
  
 for line in fileinput.input("src/CHANGELOG.html",inplace=1):
-    line = re.sub(r'#([0-9]+)', r'<a target="_blank" href="https://github.com/mjamry/youtubeScrobbler/issues/\1">#\1</a>', line.rstrip())
+    line = re.sub(r'$$([0-9]+)', r'<a target="_blank" href="https://github.com/mjamry/youtubeScrobbler/issues/\1">#\1</a>', line.rstrip())
     print(line)
